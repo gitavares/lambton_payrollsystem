9e31ebd9720ee511bdb87c148bcba56c916102af
Giselle Tavares <gitavares@hotmail.com>
2018-09-08 21:14:02 -0400

    project created and protocol IPrintable implemented

---
+++ Payroll System/Payroll System/IPrintable.swift
@@ -0,0 +1,13 @@
+//
+//  IPrintable.swift
+//  Payroll System
+//
+//  Created by Giselle Tavares on 08/09/18.
+//  Copyright © 2018 Giselle Tavares. All rights reserved.
+//
+
+import Foundation
+
+protocol IPrintable {
+    func printMyData() -> String
+}
---
+++ Payroll System/Payroll System/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Payroll System
+//
+//  Created by Giselle Tavares on 08/09/18.
+//  Copyright © 2018 Giselle Tavares. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
