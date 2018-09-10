fc6fcc44eda4a820648c5bcc0a25db12f30d3d9a
Giselle Tavares <gitavares@hotmail.com>
2018-09-10 16:10:15 -0400

    initial structure

---
+++ Payroll System/Payroll System/CommissionBasedPartTime.swift
@@ -0,0 +1,13 @@
+//
+//  CommissionBasedPartTime.swift
+//  Payroll System
+//
+//  Created by Giselle Tavares on 10/09/18.
+//  Copyright © 2018 Giselle Tavares. All rights reserved.
+//
+
+import Foundation
+
+class CommissionBasedPartTime: PartTime {
+    
+}
