d76c9f070c58342d288d2bc7fae1170df110e1c4
Giselle Tavares <gitavares@hotmail.com>
2018-09-17 20:08:28 -0400

    created the gitignore file

---
+++ AGG_MAD3004_FinalProject/AGG_MAD3004_FinalProject/Exceptions.swift
@@ -0,0 +1,9 @@
+//
+//  Exceptions.swift
+//  AGG_MAD3004_FinalProject
+//
+//  Created by Giselle Tavares on 17/09/18.
+//  Copyright © 2018 Giselle Tavares. All rights reserved.
+//
+
+import Foundation
